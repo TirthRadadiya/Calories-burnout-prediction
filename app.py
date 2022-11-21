d4ba2e0403f91ddae91781007eefc1941be9e6db
TirthRadadiya <radadiyatirth@gmail.com>
2022-11-21 18:11:49 +0530

    fixed deployment issue

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
 from flask import Flask
-from cbp.exception import HousingException
-from cbp.logger import logging
 
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    try:
-        logging.INFO("Logging is working and we are in try block")
-        raise Exception("This is trial Exception")
-    except Exception as e:
-        # logging.INFO("loggin in Except block")
-        raise HousingException(e, sys) from 
     return "Starting of Machine Learning End to End Project"
 
 
